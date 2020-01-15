9aec14dc33350a9e7571c9561e17beb0a431480f
jakeoeding <jaoeding@seas.upenn.edu>
2020-01-14 19:02:10 -0600

    initial creation of Queue and related tests

---
+++ src/Queue.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace DataStructures
+{
+    public class Queue<T>
+    {
+        private int _head;
+        private int _tail;
+        private T[] _array;
+        public int Count { get; private set; }
+        public const int DefaultInitialCapacity = 20;
+        public const int DefaultGrowthFactor = 2;
+
+        public Queue()
+        {
+            _head = 0;
+            _tail = 0;
+            _array = new T[DefaultInitialCapacity];
+            Count = 0;
+        }
+
+        public Queue(int initialCapacity)
+        {
+            _head = 0;
+            _tail = 0;
+            _array = new T[initialCapacity];
+            Count = 0;
+        }
+
+        public void Enqueue(T item)
+        {
+            if (Count == _array.Length)
+            {
+                Resize();
+            }
+            _array[_tail] = item;
+            AdvancePosition(ref _tail);
+            Count++;
+        }
+
+        public T Peek()
+        {
+            if (Count == 0)
+            {
+                ThrowWhenEmpty();
+            }
+            return _array[_head];
+        }
+
+        public T Dequeue()
+        {
+            if (Count == 0)
+            {
+                ThrowWhenEmpty();
+            }
+            T frontOfQueue = _array[_head];
+            _array[_head] = default(T);
+            AdvancePosition(ref _head);
+            Count--;
+            return frontOfQueue;
+        }
+
+        public void Clear()
+        {
+            Array.Clear(_array, 0, _array.Length);
+            Count = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            return Count > 0 && Array.IndexOf(_array, item) > -1;
+        }
+
+        public T[] ToArray()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Resize()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AdvancePosition(ref int position)
+        {
+            position = (position + 1) % _array.Length;
+        }
+
+        private void ThrowWhenEmpty()
+        {
+            throw new InvalidOperationException("Queue is empty. This operation requires at least one item to be present in Queue.");
+        }
+    }
+}
---
+++ tests/QueueTests.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using NUnit.Framework;
+using DataStructures;
+
+namespace DataStructuresTests
+{
+    [TestFixture]
+    public class QueueTests
+    {
+        Queue<int> IntQueue;
+
+        [SetUp]
+        public void Setup()
+        {
+            IntQueue = new Queue<int>();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            IntQueue = null;
+        }
+
+        #region Enqueue tests
+
+        [Test]
+        public void EnqueueShouldIncreaseCountByOne()
+        {
+            int initialCount = IntQueue.Count;
+            IntQueue.Enqueue(1);
+            Assert.AreEqual(initialCount + 1, IntQueue.Count);
+        }
+
+        #endregion
+
+        #region Peek tests
+
+        [Test]
+        public void PeekShouldThrowInvalidOperationExceptionWhenQueueEmpty()
+        {
+            Assert.Throws<InvalidOperationException>(() => IntQueue.Peek());
+        }
+
+        [Test]
+        public void PeekShouldReturnFirstValueInQueue()
+        {
+            IntQueue.Enqueue(1);
+            IntQueue.Enqueue(2);
+            Assert.AreEqual(IntQueue.Peek(), 1);
+        }
+
+        [Test]
+        public void PeekShouldNotAffectCount()
+        {
+            IntQueue.Enqueue(1);
+            IntQueue.Enqueue(2);
+            IntQueue.Peek();
+            Assert.AreEqual(IntQueue.Count, 2);
+        }
+
+        #endregion
+
+        #region Dequeue tests
+
+        [Test]
+        public void DequeueShouldThrowInvalidOperationExceptionWhenQueueEmpty()
+        {
+            Assert.Throws<InvalidOperationException>(() => IntQueue.Dequeue());
+        }
+
+        [Test]
+        public void DequeueShouldReturnFirstValueInQueue()
+        {
+            IntQueue.Enqueue(1);
+            IntQueue.Enqueue(2);
+            Assert.AreEqual(IntQueue.Dequeue(), 1);
+        }
+
+        [Test]
+        public void DequeueShouldDecreaseCountByOne()
+        {
+            IntQueue.Enqueue(1);
+            IntQueue.Enqueue(2);
+            IntQueue.Dequeue();
+            Assert.AreEqual(IntQueue.Count, 1);
+        }
+
+        #endregion
+
+        #region Clear tests
+
+        [Test]
+        public void ClearShouldResetCountToZero()
+        {
+            for (int i = 1; i < 4; i++)
+            {
+                IntQueue.Enqueue(i);
+            }
+            IntQueue.Clear();
+            Assert.AreEqual(IntQueue.Count, 0);
+        }
+
+        #endregion
+
+        #region Contains tests
+
+        [Test]
+        public void ContainsShouldReturnTrueWhenQueueContainsValue()
+        {
+            IntQueue.Enqueue(10);
+            Assert.IsTrue(IntQueue.Contains(10));
+        }
+
+        [Test]
+        public void ContainsShouldReturnFalseWhenStackDoesntContainValue()
+        {
+            IntQueue.Enqueue(10);
+            Assert.IsFalse(IntQueue.Contains(15));
+        }
+
+        [Test]
+        public void ContainsShouldReturnFalseWhenCountIsZero()
+        {
+            Assert.IsFalse(IntQueue.Contains(10));
+        }
+
+        #endregion
+    }
+}
