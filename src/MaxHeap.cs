4a93cac6579552ee674076f3812758a15f3d0d29
jakeoeding <jaoeding@seas.upenn.edu>
2020-06-27 17:02:49 -0500

    implemented MaxHeap and added MaxHeap tests

---
+++ src/MaxHeap.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DataStructures
+{
+    public class MaxHeap<T> : MinHeap<T> where T : IComparable
+    {
+        #region public interface
+
+        public MaxHeap() : base() { }
+
+        public MaxHeap(int initialCapacity) : base(initialCapacity) { }
+
+        #endregion
+
+        #region internal helpers
+
+        protected override bool Precedes(int x, int y)
+        {
+            return _array[x].CompareTo(_array[y]) > 0;
+        }
+
+        #endregion
+    }
+}
---
+++ tests/MaxHeapTests.cs
@@ -0,0 +1,81 @@
+﻿using NUnit.Framework;
+using DataStructures;
+
+namespace DataStructuresTests
+{
+    [TestFixture]
+    public class MaxHeapTests
+    {
+        MaxHeap<int> IntMaxHeap;
+
+        [SetUp]
+        public void Setup()
+        {
+            IntMaxHeap = new MaxHeap<int>();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            IntMaxHeap = null;
+        }
+
+        #region Add tests
+
+        [Test]
+        public void AddShouldPutMaxItemOnTop()
+        {
+            IntMaxHeap.Add(5);
+            IntMaxHeap.Add(10);
+            Assert.AreEqual(10, IntMaxHeap.Peek());
+        }
+
+        [Test]
+        public void AddShouldLeaveTopUnchangedIfNewItemLesser()
+        {
+            IntMaxHeap.Add(10);
+            IntMaxHeap.Add(5);
+            Assert.AreEqual(10, IntMaxHeap.Peek());
+        }
+
+        #endregion
+
+        #region Peek tests
+
+        [Test]
+        public void PeekShouldReturnMaxValue()
+        {
+            IntMaxHeap.Add(10);
+            IntMaxHeap.Add(5);
+            Assert.AreEqual(10, IntMaxHeap.Peek());
+        }
+
+        #endregion
+
+        #region RemoveTop tests
+
+         [Test]
+        public void RemoveTopShouldActuallyRemoveTheTopFromHeap()
+        {
+            IntMaxHeap.Add(10);
+            IntMaxHeap.Add(5);
+            Assert.AreEqual(10, IntMaxHeap.RemoveTop());
+        }
+
+        [Test]
+        public void RemoveTopShouldRemoveItemsInSortedOrder()
+        {
+            int[] testInts = { -3, -2, -1, 0, 1, 2, 3 };
+            foreach (int testInt in testInts)
+            {
+                IntMaxHeap.Add(testInt);
+            }
+            for (int i = testInts.Length - 1; i < -1; i--)
+            {
+                Assert.AreEqual(testInts[i], IntMaxHeap.RemoveTop());
+            }
+        }
+
+        #endregion
+    }
+}
